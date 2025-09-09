6d78ba211ae1b74b8d8c1cdb0d297c532cfe0a24
Andrés Núñez <91515514+AndresNunez11@users.noreply.github.com>
2025-09-08 22:35:05 -0600

    Update vite.config.js
    Actualizar Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React_API_Simpson/',   // 👈 usa el nombre de tu repo
 })
+
